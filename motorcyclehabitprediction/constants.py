babd33ab7589853efa1c98d589fc628794aa1013
igor-cheb <imadmeister@gmail.com>
2023-05-22 14:02:42 +0600

    added constants and etl class

---
+++ motorcyclehabitprediction/constants.py
@@ -0,0 +1,30 @@
+# list of all usable cols
+ALL_COLS = [
+    'T_min',
+    'T_mean',
+    'T_max',
+    'P_min',
+    'P_mean',
+    'P_max',
+    'U_min',
+    'U_mean',
+    'U_max',
+    'Ff_min',
+    'Ff_mean',
+    'Ff_max',
+    'N_min',
+    'N_mean',
+    'N_max',
+    'VV_min',
+    'VV_mean',
+    'VV_max',
+    'RRR_min',
+    'RRR_mean',
+    'RRR_max'
+]
+
+# name of label
+LABEL = 'y'
+
+# num of PCA output cols
+PCA_OUT_NUM = 10
