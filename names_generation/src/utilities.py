cf20ddc1e82d63efe58943a63a57955fe6f99a5e
igor-cheb <imadmeister@gmail.com>
2023-05-25 08:21:57 +0600

    rnn draft created

---
+++ names_generation/src/utilities.py
@@ -0,0 +1,33 @@
+# all symbols tb used for modelling
+vocabulary = [
+    '.',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+]
+
+# length of data sample
+CONTEXT_LEN = 10
